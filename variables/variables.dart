8ee827e1dbb8ed098fde4744f9f4072a0aa2a62c
ahmednadeem0 <nadeemahmed01411@gmail.com>
2026-03-05 12:33:16 +0500

    variables

---
+++ variables/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Nadeem";
+  int age = 22;
+  double height = 5.9;
+  bool isStudent = true;
+
+  print(name);
+  print(age);
+  print(height);
+  print(isStudent);
+}
